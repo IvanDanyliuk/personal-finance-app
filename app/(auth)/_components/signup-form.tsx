e5858731ab5604bf76404905ec145cbc6261c628
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-11-22 13:45:01 +0200

    the uploadthing setup

--- app/(auth)/_components/signup-form.tsx
+++ app/(auth)/_components/signup-form.tsx
@@ -1,12 +1,35 @@
-import { CheckboxField } from "@/components/inputs/Checkbox";
-import { TextField } from "@/components/inputs/TextField";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+'use client';
+
+import { useFormState } from 'react-dom';
+import Link from 'next/link';
+import { CheckboxField } from '@/components/inputs/Checkbox';
+import FileInput from '@/components/inputs/FileInput';
+import { TextField } from '@/components/inputs/TextField';
+import { Button } from '@/components/ui/button';
+import { signup } from '@/lib/actions/user.actions';
+import { useState } from 'react';
+
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  image: ''
+};
+
 
 export const SignUpForm = () => {
+  const [state, formAction] = useFormState<any, any>(signup, initialState);
+  const [isAgreementConfirmed, setIsAgreementConfirmed] = useState<boolean>(false);
+
+  const handleAgreementConfirm = () => {
+    setIsAgreementConfirmed(!isAgreementConfirmed);
+  };
+
   return (
     <form 
-      action='' 
+      action={formAction} 
       className='w-full flex flex-col justify-center gap-3'
     >
       <TextField 
@@ -29,8 +52,12 @@ export const SignUpForm = () => {
         name='confirmPassword'
         type='confirmPassword'
       />
+      <FileInput 
+        name='image'
+      />
       <Button 
         type='submit' 
+        disabled={!isAgreementConfirmed}
         className='w-full py-6 mt-3 bg-primary-7 hover:bg-primary-6 rounded-full text-white font-semibold'
       >
         Sign In
@@ -39,6 +66,8 @@ export const SignUpForm = () => {
         <CheckboxField 
           name='agreement'
           label='I agree with'
+          checked={isAgreementConfirmed}
+          onChange={handleAgreementConfirm}
         />
         <Link 
           href='/privacy-policy' 
