981818690bf1092dadc6711e251bd6fc69acefd8
IvanDanyliuk <ivandaniliuk@gmail.com>
2025-01-18 19:59:07 +0200

    customize the progress bar component

--- app/(application)/analytics/page.tsx
+++ app/(application)/analytics/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { Progress } from "@/components/ui/progress";
+
 export default function AnalyticsPage() {
   return (
     <div>
       Analytics
+      <Progress value={78} />
     </div>
   );
 };
